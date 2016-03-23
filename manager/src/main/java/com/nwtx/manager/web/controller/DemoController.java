fcbbb3a6ced3bd4f88543d4d07c6a0d39d480a1d
liuhui3 <liuhui3@meilele.com>
2016-03-23 14:56:24 +0800

    init commit

--- manager/src/main/java/com/nwtx/manager/web/controller/DemoController.java
+++ manager/src/main/java/com/nwtx/manager/web/controller/DemoController.java
@@ -1,11 +1,11 @@
 package com.nwtx.manager.web.controller;
 
-import com.nwtx.manager.common.exception.ControllerException;
 import com.nwtx.manager.data.entity.MchBasicInfo;
 import com.nwtx.manager.web.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by liuhui on 2016/3/23.
@@ -16,11 +16,10 @@ public class DemoController {
     @Autowired
     private DemoService demoService;
 
-    @RequestMapping("index")
+    @RequestMapping(value = "index", method = RequestMethod.GET)
     public String index(){
         MchBasicInfo mchBasicInfo = demoService.getData("2");
         System.out.println(mchBasicInfo);
-        throw new ControllerException(0,"error");
-//        return "index";
+        return "index";
     }
 }
